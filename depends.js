0fa58735a7d68350a90580e4677bf22ea412f945
Leander Hanwald <l.hanwald@interactivecms.de>
2011-10-19 16:22:55 +0200

    Now a webos app

--- depends.js
+++ depends.js
@@ -1,9 +1,2 @@
 enyo.depends(
-     "images/bg.png",
-     "images/fill_inactive.png",
-     "images/fill.png",
-     "images/marked.png",
-     "images/mark_inactive.png",
-     "images/mark.png",
-     "images/mistake.png"
 );
